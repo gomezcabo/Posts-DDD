1cca854985e978af6b2053859125d3e6265b44b3
Juan Gómez <gomezcabo@gmail.com>
2023-10-26 09:22:23 +0100

    rename hook

--- src/infrastructure/ui/hooks/use-post-by-id.ts
+++ src/infrastructure/ui/hooks/use-post-by-id.ts
@@ -2,6 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { Post } from "../../../domain/models/post";
 import { PostService } from "../../../domain/services";
 
-export function usePost(postId: Post["id"]) {
+export function usePostById(postId: Post["id"]) {
   return useQuery(["posts", postId], () => PostService.getPostByIdWithCommentsAndUsers(postId));
 }
