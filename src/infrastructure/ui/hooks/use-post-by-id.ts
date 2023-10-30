9e162a20ca912c474ccde233e172166c1bbf5b00
Juan Gómez <gomezcabo@gmail.com>
2023-10-30 16:29:55 +0000

    minor

--- src/infrastructure/ui/hooks/use-post-by-id.ts
+++ src/infrastructure/ui/hooks/use-post-by-id.ts
@@ -4,6 +4,6 @@ import { GetPostByIdUseCase } from "../../../domain/use-cases/get-post-by-id";
 import container from "../../../inversify";
 
 export function usePostById(postId: Post["id"]) {
-  const getPostByIdUseCase = container.get<GetPostByIdUseCase>(GetPostByIdUseCase);
+  const getPostByIdUseCase = container.get(GetPostByIdUseCase);
   return useQuery(["posts", postId], () => getPostByIdUseCase.execute(postId));
 }
