16524b39f1b63b07213824a7ec7bec9a0f7af6d1
Juan Gómez <gomezcabo@gmail.com>
2023-11-06 10:42:42 +0000

    add comments to ui code

--- src/infrastructure/ui/hooks/use-post-by-id.ts
+++ src/infrastructure/ui/hooks/use-post-by-id.ts
@@ -5,6 +5,7 @@ import { PostRepository } from "../../repositories/post/post.repository";
 import { GetPostByIdUseCase } from "../../../domain/use-cases/get-post-by-id";
 
 export function usePostById(postId: Post["id"]) {
+  // manually create all the dependencies
   const apiAdapter = new ApiAdapter();
   const postRepository = new PostRepository(apiAdapter);
   const getPostByIdUseCase = new GetPostByIdUseCase(postRepository);
--- src/infrastructure/ui/hooks/use-posts.ts
+++ src/infrastructure/ui/hooks/use-posts.ts
@@ -2,5 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { PostUseCases } from "../../../domain/use-cases";
 
 export function usePosts() {
+  // get the instance from the singleton PostUseCases where they are created only once
   return useQuery(["posts"], () => PostUseCases.GetPostsUseCase.execute());
 }
